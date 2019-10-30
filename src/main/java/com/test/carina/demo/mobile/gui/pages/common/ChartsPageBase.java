cd30be6d98f23ec95181bfb16bfdfadefb69a16b
sshukalovich <sshukalovich@consultant.underarmour.com>
2019-10-30 14:01:49 +0300

    init

---
+++ src/main/java/com/test/carina/demo/mobile/gui/pages/common/ChartsPageBase.java
@@ -0,0 +1,12 @@
+package com.test.carina.demo.mobile.gui.pages.common;
+
+import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+
+public class ChartsPageBase extends AbstractPage {
+
+    public ChartsPageBase(WebDriver driver) {
+        super(driver);
+    }
+
+}
